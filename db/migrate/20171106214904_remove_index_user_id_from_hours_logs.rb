a85a0af8a824eea0e4bc06d84401df9a23c36387
Jeremy Oglesby <iaminaband@gmail.com>
2017-11-10 09:34:18 -0700

    Fix migration that is causing Heroku issues (hopefully)

--- db/migrate/20171106214904_remove_index_user_id_from_hours_logs.rb
+++ db/migrate/20171106214904_remove_index_user_id_from_hours_logs.rb
@@ -1,6 +1,5 @@
 class RemoveIndexUserIdFromHoursLogs < ActiveRecord::Migration[5.1]
   def change
-    remove_index :hours_logs, :user_id
     remove_column :hours_logs, :user_id, :integer
   end
 end
